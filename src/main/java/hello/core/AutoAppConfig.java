a2b82c0f26a882b2a7ebece7d11f91c91fbc3908
kang hyeon <hyeonkang.dev@gmail.com>
2022-08-01 16:58:26 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // @Component이 붙은 클래스를 자동으로 스프링빈으로 등록해준다.
+        basePackages = "hello.core",    // hello.core 부터 찾아들어간다.
+        basePackageClasses = AutoAppConfig.class,   // AutoAppConfig의 패키지는 현재 hello.core 이므로 hello.core부터 찾아들어간다.
+        // 지정하지 않으면 @ComponentScan을 붙이는 클래스의 패키지 (현재 hello.core) 부터 시작하는게 디폴트이다.
+
         // @Configuration에 @Component가 포함되어있기 때문에 파일을 제외해준다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
